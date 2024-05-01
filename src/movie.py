c6ff15e2804514e3547cad4459b948995531f3b9
kuri884 <krc.kuribayashi@gmail.com>
2024-05-01 17:21:58 +0900

    動画に画像を合成して書き出すことができる

--- src/movie.py
+++ src/movie.py
@@ -1,13 +1,20 @@
 import moviepy.editor as mpe
+import cv2
 from Variables import *
 
 path = f'{MATERIALS}/{FILENAME}'
 video = mpe.VideoFileClip(path)
+background = mpe.ImageClip(f'{MATERIALS}/back.png').resize(video.size)
 
-masked_video = mpe.vfx.mask_color(video, color=COLOR, thr=THR, s=S)
+chroma_video = video.fx(mpe.vfx.mask_color, color=COLOR, thr=THR, s=S)
+
+final_video = mpe.CompositeVideoClip([
+    background.set_duration(video.duration),
+    chroma_video.set_duration(video.duration)
+])
 
 out_path = f'{OUT_DIR}/{OUT_FILE}'
-output = masked_video.write_videofile(
+output = final_video.write_videofile(
     out_path,
     codec="libx264",
     audio_codec="aac",
