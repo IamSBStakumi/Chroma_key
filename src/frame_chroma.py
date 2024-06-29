bd72e620735d6ef9cc3b8cca010240006bb659eb
IamSBStakumi <bruewhite256@gmail.com>
2024-06-29 20:01:01 +0900

    動画出力までは可能．クロマキー画像は出力できるが，クロマキー動画にはなっていない

--- src/frame_chroma.py
+++ src/frame_chroma.py
@@ -7,44 +7,76 @@ from PIL import Image
 
 from Variables import *
 
+# パラメータ設定
+contrast_adjustment_value = 1.5  # コントラスト調整値
+chroma_key_color = np.uint8([[[0, 255, 0]]])  # クロマキー処理の指定色（緑色）
+chroma_key_threshold = 20  # クロマキー処理の閾値
+noise_removal_iterations = 50  # ノイズ除去の繰り返し回数
+
+# 動画と背景画像の読込
 path = f'{MATERIALS}/{FILENAME}'
 video = cv2.VideoCapture(path)
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 background = mpe.ImageClip(f'{MATERIALS}/back.jpg').resize((width, height))
 
-for i in range(1): # 1フレーム分を処理
+# 動画の総フレーム数を取得
+frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+# 書き出し用のwriteクラスを作成
+fps = video.get(cv2.CAP_PROP_FPS)
+fmt = cv2.VideoWriter.fourcc(*"mp4v")
+writer = cv2.VideoWriter("outputs/chroma.mp4", 0x00000020, fps, (width, height), 0)
+
+# 音声トラック書き出し
+clip_input = mpe.VideoFileClip(path)
+clip_input.audio.write_audiofile("outputs/audio.mp3")
+
+def create_frame(input_frame):
+    # コントラスト調整
+    contrast_image = cv2.convertScaleAbs(input_frame, alpha=contrast_adjustment_value, beta=0)
+
+    # クロマキー処理と二値化
+    hsv_chroma_key_color = cv2.cvtColor(chroma_key_color, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([hsv_chroma_key_color[0][0][0] - chroma_key_threshold, 50, 50])
+    upper_green = np.array([hsv_chroma_key_color[0][0][0] + chroma_key_threshold, 255, 255])
+    hsv_image = cv2.cvtColor(contrast_image, cv2.COLOR_BGR2HSV)
+    chroma_key_image = cv2.inRange(hsv_image, lower_green, upper_green)
+    mask_image = cv2.bitwise_not(chroma_key_image)
+
+    # ノイズ除去
+    result_image = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA) # RGBA形式に変換
+    result_image[:, :, 3] = mask_image # アルファチャンネルにマスク画像を設定
+    
+    return result_image
+
+
+
+# for i in range(1): # 1フレーム分を処理
+for i in range(frame_count):
     success, frame = video.read()
 
     if not success:
-        print(i, "failed")
-        continue
-
-    # HSV変換
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # 2値化する
-    bin_img = ~cv2.inRange(hsv_frame, (62, 100, 0), (79, 255, 255))
+        print("failed reading")
+        break
     
-    # 輪郭抽出
-    contours, _ = cv2.findContours(bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    chroma_frame = create_frame(frame)
 
-    # 面積が最大の輪郭を取得する
-    contour = max(contours, key=lambda x: cv2.contourArea(x))
+    # 画像を動画へ書き出し
+    writer.write(chroma_frame)
 
-    # マスク画像を作成
-    mask = np.zeros_like(bin_img)
-    cv2.drawContours(mask, [contour], -1, color=255, thickness=-1)
-    
-    # 合成
-    dst = np.where(mask[:height, :width, np.newaxis] == 0, background, frame)
-    out = 'chroma_frame{}.png'.format(str(i).zfill(3))
+# 読み込んだ動画と書き出し先の動画を開放
+video.release()
+writer.release()
 
-    print(dst)
+# 音声トラックを動画に追加
+clip = mpe.VideoFileClip("outputs/chroma.mp4").subclip()
+clip.write_videofile("outputs/result.mp4", audio="outputs/audio.mp3")
 
-    out_img = Image.fromarray(dst)
+print("finish!")
 
-    out_img.save("outputs/f_img.png")
-    # cv2.imwrite(os.path.join("outputs", out), Image(dst))
-
-    # https://pystyle.info/opencv-mask-image/
+    # 画像保存
+    # cv2.imwrite(os.path.join("outputs/contrast.png"), contrast_image)
+    # cv2.imwrite(os.path.join("outputs/chromakey.png"), chroma_key_image)
+    # cv2.imwrite(os.path.join("outputs/mask.png"), mask_image)
+    # cv2.imwrite(os.path.join("outputs/result.png"), result_image)
