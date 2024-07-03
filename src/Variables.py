efe48393c0e46f3c002a89a9cf15b88f285a6565
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2024-07-03 19:25:34 +0900

    使用しないファイルやパラメータを削除

--- src/Variables.py
+++ src/Variables.py
@@ -14,11 +14,3 @@ chroma_key_color = np.uint8([[[0, 255, 0]]])  # クロマキー処理の指定�
 chroma_key_threshold = 20  # クロマキー処理の閾値
 noise_removal_iterations = 50  # ノイズ除去の繰り返し回数
 
-# 透明度の計算式：d**s / (thr**s + d**s) (d: COLORとの距離、色差)
-# d>>thr の場合は 1、d<<thr の場合は 0に近づく
-# COLOR: マスクする色
-# THR: しきい値
-# S: 
-COLOR = [63,94,72]
-THR = 0
-S = 1
